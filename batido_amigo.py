c08d847678b737cfa721d0efcac6da2a9ea64556
Hernán Giayetto <hernangiayetto@gmail.com>
2018-12-11 00:08:52 -0300

    Added --restricted-years arg Check restrictions of results Print results on test Write results on real run Use google sheets as db

--- batido_amigo.py
+++ batido_amigo.py
@@ -1,6 +1,16 @@
 
+import os
 import argparse
 from datetime import datetime
+from random import sample
+from db_api import get_players, get_players_last_results, get_player_names
+
+MAX_ATTEMPTS = 500
+OUT_DIRECTORY_TEMPL = "./batido_{players}_{event}_{year}/"
+RESULT_TEXT_TEMPL = "batido_texto_{players}_{event}_{year}.txt"
+RESULT_FILE_TEMPL = "{player}.txt"
+RESULTS_FILE = "Resultados.txt"
+RESULTS_HEADER_TEMPLATE = "results_header_template.txt"
 
 now = datetime.now()
 
@@ -15,22 +25,100 @@ parser.add_argument("-p", "--players",
                     choices=['giayetto', 'frutos'],
                     required=True)
 parser.add_argument("-y", "--year",
-                    help="The year this batido will be played on",
+                    help="The year this batido will be played on. Default is current year",
+                    type=int,
                     default=now.year)
+parser.add_argument("-r", "--restricted-years",
+                    help="Results must be different from the last RESTRICTED_YEARS. Default is 2 years",
+                    type=int,
+                    default=2)
 parser.add_argument("-t", "--test",
                     help="Whether this batido is a test or not",
                     action="store_true")
 
 args = parser.parse_args()
 
-# print args.event
-# print args.players
-# print args.year
-# print args.test
 
-# 1. get players from google
-# 2. get players restrictions from google
-# 3. hacer el batido
-# 4. chequear que se cumplan las restricciones
-# 5. si success: escribir los archivos con los resultados
+def meet_restrictions(results, restrictions, restrict_myself=True):
+  # print "meet_restrictions({}, {}, {})".format(results, restrictions, restrict_myself)
+  for f in results:
+    t = results[f]
+    if restrict_myself and f == t:
+      print "{} cannot give a present to himself/herself".format(f)
+      return False
+    if t in restrictions[f]:
+      print "{} cannot give a present to {}".format(f, t)
+      return False
+  return True
 
+
+def print_results(results):
+  for r in results:
+    print "{}\t->\t{}".format(str(player_names[r]), str(player_names[results[r]]))
+
+
+def write_results(results, directory):
+  with open(RESULTS_HEADER_TEMPLATE, 'r') as results_templ_h:
+    results_template = results_templ_h.read()
+
+    results_file = os.path.join(directory, RESULTS_FILE)
+    with open(results_file, 'w') as results_file_h:
+      results_file_h.write(results_template.format(players=args.players,
+                                                   event=args.event,
+                                                   year=args.year,
+                                                   years_ago=args.restricted_years))
+      template_name = RESULT_TEXT_TEMPL.format(players=args.players,
+                                               event=args.event,
+                                               year=args.year)
+      with open(template_name, 'r') as templ:
+        template = templ.read()
+
+        for r in results:
+          result_file = os.path.join(directory,
+                                     RESULT_FILE_TEMPL.format(player=player_names[r]))
+          print "Writting result for {} on {}".format(player_names[r], result_file)
+
+          results_file_h.write("{} -> {}\n".format(player_names[r], player_names[results[r]]))
+
+          with open(result_file, 'w') as result_file_h:
+            result_file_h.write(template.format(player=player_names[r], amaigo=player_names[results[r]]))
+
+
+if args.test:
+  print "Esto es una prueba!"
+else:
+  print "Esto NO es una prueba!!! es de verdad!!"
+
+# get players from google
+players = get_players(args.players, args.event, args.year)
+
+player_names = get_player_names(args.players, players)
+
+# get players restrictions from google
+last_results = get_players_last_results(args.players, args.event, args.year, args.restricted_years)
+
+# hacer el batido
+batido_amigo = {}
+for i in range(1, MAX_ATTEMPTS):
+  print "Attempt {}/{}".format(i, MAX_ATTEMPTS)
+  batido = sample(players, len(players))
+  results_candidate = {k:batido[i] for i,k in enumerate(players)}
+
+  # chequear que se cumplan las restricciones
+  if meet_restrictions(results_candidate, last_results):
+    # si success: escribir los archivos con los resultados
+    batido_amigo = results_candidate
+    break
+
+if batido_amigo:
+  if args.test:
+    print_results(batido_amigo)
+  else:
+    out_directory = OUT_DIRECTORY_TEMPL.format(players=args.players,
+                                               event=args.event,
+                                               year=args.year)
+    if not os.path.exists(out_directory):
+      os.mkdir(out_directory)
+
+    print "Writting results on {}".format(out_directory)
+    write_results(batido_amigo, out_directory)
---
+++ db_api.py
@@ -0,0 +1,54 @@
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+YEAR_HEADER = 'year'
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+spread = client.open("Batido")
+# sheet = spread.sheet1
+
+
+def get_players(players_name, event, year):
+  # print "get_players({}, {}, {})".format(players_name, event, year)
+  sheet = spread.worksheet("players-{}-{}".format(event, players_name))
+  all_records = sheet.get_all_records()
+  year_row = filter(lambda r : _get_row_by_year(r, year), all_records)[0]
+  players = {k:v for (k, v) in year_row.items() if v and int(v) == 1}
+  return list(players.keys())
+
+
+def get_player_names(players_name, players):
+  # print "get_player_names({}, {})".format(players_name, players)
+  sheet = spread.worksheet("{}".format(players_name))
+  all_records = sheet.get_all_records()
+  return {k['id']:k['name'] for k in all_records if k['id'] in players}
+
+
+def get_players_last_results(players_name, event, year, years_ago):
+  # print "get_players_last_results({}, {}, {}, {})".format(players_name, event, year, years_ago)
+  sheet = spread.worksheet("resultados-{}-{}".format(event, players_name))
+  all_records = sheet.get_all_records()
+
+  results = {}
+  for y in range(year-1, year-1-years_ago, -1):
+    year_results = filter(lambda r : _get_row_by_year(r, y), all_records)[0]
+    for player in year_results:
+      if player == YEAR_HEADER:
+        continue
+      if player not in results:
+        results[player] = []
+      results[player].append(year_results[player])
+
+  return results
+
+
+def _get_row_by_year(row, year):
+    return row['year'] == year
